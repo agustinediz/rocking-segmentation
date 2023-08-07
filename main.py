4ebbd92bd8ea81b4e38f4ff6ed76a64693054811
agustinediz <65187706+agustinediz@users.noreply.github.com>
2023-08-07 16:06:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ if st.button('I want new music!'):
     a = recommender["cluster"].to_numpy().astype(int)
     cluster = a[0]
     recommendation = data[data["cluster"] == cluster]
-    st.write(f"You should explore this genres: {recommendation["artist"].head(2)}")
+    st.write(f"You should explore this artists: {recommendation["artist"].head(2)}")
