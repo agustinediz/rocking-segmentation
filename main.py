f1cd5d75c9293052d636aff7bdcade342631d562
agustinediz <65187706+agustinediz@users.noreply.github.com>
2023-08-07 16:05:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ data = pd.read_csv("clustered.csv")
 #Features Zones
 st.subheader("Please, select your favourite genre")
 continent2 = st.sidebar.selectbox(label = "genres", options = data["artist_genres"].unique())
-continent2 = str(continent2)
+continent2 = str("continent2")
 
 #Prediction
 if st.button('I want new music!'):
     inputs1 = continent2
     recommender = data[data["artist_genres"] == inputs1]
-    cluster = recommender["cluster"].unique()
+    a = recommender["cluster"].to_numpy().astype(int)
+    cluster = a[0]
     recommendation = data[data["cluster"] == cluster]
-    st.write(f"You should explore this genres: {recommendation.head(2)}")
+    st.write(f"You should explore this genres: {recommendation["artist"].head(2)}")
